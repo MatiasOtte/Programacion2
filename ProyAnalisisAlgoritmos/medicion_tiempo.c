ea1f81800e50af85cdec41c57d5aeb82ed456ebf
MatiasOtte <ottematias@gmail.com>
2023-04-11 17:32:08 -0300

    commit

---
+++ ProyAnalisisAlgoritmos/medicion_tiempo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+#include <stdlib.h>
+
+int main(){
+    int n;
+    do{
+        printf("Ingrese un numero entero positivo:\n");
+        scanf("%d",&N);
+    }while(N<0);
+    
+    for(int contador=0;contador++; contador<=N){
+        contador=contador+1;
+    }
+
+    system("pause");    
+    return 0;
+}
